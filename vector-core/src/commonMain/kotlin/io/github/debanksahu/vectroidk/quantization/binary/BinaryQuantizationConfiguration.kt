01c53eceb423837611d57b795b03e093fba05985
Deban Kumar Sahu <sahudebankumar@gmail.com>
2026-06-12 19:48:40 +0530

    refactor: Added inputSize parameter into BinaryQuantizationConfiguration data class. - In future this parameter will be used for a stable collection system.

--- vector-core/src/commonMain/kotlin/io/github/debanksahu/vectroidk/quantization/binary/BinaryQuantizationConfiguration.kt
+++ vector-core/src/commonMain/kotlin/io/github/debanksahu/vectroidk/quantization/binary/BinaryQuantizationConfiguration.kt
@@ -3,6 +3,7 @@ package io.github.debanksahu.vectroidk.quantization.binary
 import io.github.debanksahu.vectroidk.quantization.QuantizationConfiguration
 
 data class BinaryQuantizationConfiguration(
+    val inputSize: Int = 256,
     val blockSize: Int = 32,
     val variation: BinaryVariation = BinaryVariation.MEAN_THRESHOLD
 ) : QuantizationConfiguration
