53b93d61c8c3821d30f3479c615fb37b2c2c1b23
Deban Kumar Sahu <sahudebankumar@gmail.com>
2026-06-13 22:26:10 +0530

    feat: Added `inputSize` parameter into `BlockwiseQuantizationConfiguration` to make sure the input remain consistent throughout the operation.

--- vector-core/src/commonMain/kotlin/io/github/debanksahu/vectroidk/quantization/blockwise/BlockwiseQuantizationConfiguration.kt
+++ vector-core/src/commonMain/kotlin/io/github/debanksahu/vectroidk/quantization/blockwise/BlockwiseQuantizationConfiguration.kt
@@ -4,6 +4,7 @@ import io.github.debanksahu.vectroidk.quantization.QuantizationConfiguration
 import io.github.debanksahu.vectroidk.utils.enums.NormalizationMethod
 
 data class BlockwiseQuantizationConfiguration(
+    val inputSize: Int = 256,
     val blockSize: Int = 32,
     val normalizationMethod: NormalizationMethod? = null,
 ) : QuantizationConfiguration
