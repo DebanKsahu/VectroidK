8923d2357b910ad34375a820e3e322a78bd85af4
Deban Kumar Sahu <sahudebankumar@gmail.com>
2026-06-13 22:24:47 +0530

    feat: Added `convertToQuantizedOutput()` method which convert the stored ByteArray into the respective output object.

--- vector-core/src/commonMain/kotlin/io/github/debanksahu/vectroidk/quantization/Quantization.kt
+++ vector-core/src/commonMain/kotlin/io/github/debanksahu/vectroidk/quantization/Quantization.kt
@@ -1,11 +1,13 @@
 package io.github.debanksahu.vectroidk.quantization
 
-interface Quantization<T : QuantizationConfiguration, O : QuantizationOutput> {
+interface Quantization<CONFIG_TYPE : QuantizationConfiguration, OUTPUT_TYPE : QuantizationOutput> {
 
-    val config: T
+    val config: CONFIG_TYPE
 
-    fun quantize(inputVector: FloatArray): O
+    fun quantize(inputVector: FloatArray): OUTPUT_TYPE
 
-    fun calculateSimilarityScore(input1: O, input2: O): Float
+    fun calculateSimilarityScore(input1: OUTPUT_TYPE, input2: OUTPUT_TYPE): Float
+
+    fun convertToQuantizedOutput(inputVector: ByteArray, kwargs: Map<String, Any> = emptyMap()): OUTPUT_TYPE
 
 }
